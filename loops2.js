1d5c5893d6121231887e2c3ee6c0070719ee0e19
Neziha Uçar <nezihaucar6208@gmail.com>
2025-07-29 23:17:29 +0300

    the loops2.js file has been added.

---
+++ loops2.js
@@ -0,0 +1,15 @@
+let numbers = [1,2,3,4];
+
+let newNumbers = numbers.map((item) => {
+    return item*2;
+});
+
+console.log(numbers);
+console.log(newNumbers);
+
+let age = 14;
+
+while(age < 20){
+    console.log(age);
+    age += 1;
+}
